83ae8cf19b1432fb5fb55c239bf30c25a3bf4eb2
Kaylyn Wheeler <you@example.com>
2024-07-21 19:37:37 -0500

    First commit being made

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,14 @@
+package com.promineotech;
+
+public class Application {
+
+ public static void main(String[] args) {
+		System.out.println("hello world".charAt(0) != 'H');
+	}
+
+}
+   	  
+
+
+
+		
